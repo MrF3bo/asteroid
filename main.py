95208a95b94c4226afc83c4635c505cc394fd0db
MrF3bo <calm.boat2479@fastmail.com>
2024-08-30 05:31:25 +0200

    Added constants.py and created the game window with functionality to exit

--- main.py
+++ main.py
@@ -2,9 +2,21 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting asteroids!")
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # Creating a window with a black background
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+        # This allows the player to use the window's X button to exit the game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
