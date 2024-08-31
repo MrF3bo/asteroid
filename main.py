151ec428a9d5cd64b19b5f27b00b24d68da68e41
MrF3bo <calm.boat2479@fastmail.com>
2024-08-31 06:31:46 +0200

    Added framerate limit

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
-        # Creating a window with a black background
-        screen.fill((0,0,0))
-        pygame.display.flip()
+    clock = pygame.time.Clock()
+    dt = 0
 
-        # This allows the player to use the window's X button to exit the game
+    # Game loop
+    while True:
+        # Create functionality to exit out of the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Creating a window with a black background
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        
+        # Limiting framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
