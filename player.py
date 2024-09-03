1ecd70154e71945facc4e5f53caa95d7b7484c0a
Mrf3bo <calm.boat2479@fastmail.com>
2024-09-03 14:35:55 +0200

    Added rotation functionality to player

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import *
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -15,3 +15,16 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    # Rotation functionality
+    def rotate(self, dt):
+        self.rotation +=  PLAYER_TURN_SPEED * dt
+    
+    # Updating player state
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        # Assigning rotation functionality to keys
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
